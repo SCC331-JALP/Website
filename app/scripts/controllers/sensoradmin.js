1f0439cd498af2b28d63b709b48f8f3269109e8b
Joshua Stennett <Josh@dyn-10-32-156-10.winet.local>
2016-01-26 17:11:30 +0000

    initial groundwork on sensors admin panel

--- app/scripts/controllers/sensoradmin.js
+++ app/scripts/controllers/sensoradmin.js
@@ -15,3 +15,14 @@ angular.module('jalpWebApp')
       'Karma'
     ];
   });
+
+var USERNAME = "Povilas"; //username Initialize as constant for now - implement based on authentication later on.
+var userReference = new Firebase("https://sunsspot.firebaseio.com/users/" + USERNAME + "/spots")
+
+userReference.on('child_added', function(snapshot){
+    handleNewSensor(snapshot.val())
+});
+
+function handleNewSensor(snapshot){
+  console.log(snapshot);
+}
