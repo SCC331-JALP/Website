e2c297db7c515642f1c6b500ed21e8b8088f9864
Joshua Stennett <Josh@dyn-10-32-156-10.winet.local>
2016-01-26 19:49:06 +0000

    basic display of sensors

--- app/scripts/controllers/sensoradmin.js
+++ app/scripts/controllers/sensoradmin.js
@@ -25,4 +25,35 @@ userReference.on('child_added', function(snapshot){
 
 function handleNewSensor(snapshot){
   console.log(snapshot);
+
+var sensorString = "<tr> <td>" + snapshot.name + "</td>" + "<td>" + snapshot.alive + "</td>" + "<td>" + snapshot.battery + "</td>";
+
+if(snapshot.button == 0){
+  sensorString += "<td> None </td>"
+}else if(snapshot.button == 1){
+  sensorString += "<td> 1 pressed </td>"
+}else if(snapshot.button == 2){
+  sensorString += "<td> 1 up </td>"
+}else if(snapshot.button == 3){
+  sensorString += "<td> 2 pressed </td>"
+}else if(snapshot.button == 4){
+  sensorString += "<td> 2 up </td>"
+}
+
+sensorString += "<td>" + snapshot.light + "</td>" + "<td>" + snapshot.temp + "</td>" + "<td>" + snapshot.compass + "</td>" + "<td>" + snapshot.infrared + "</td>" + "<td>" + snapshot.sound + "</td>" + "</tr>";
+
+
+  $("#sensorTable").append(sensorString)
+
+
+
+
+  increaseSensorCount();
+
+}
+
+
+function increaseSensorCount(){
+  $("#sensorCount")[0].innerHTML++
+
 }
