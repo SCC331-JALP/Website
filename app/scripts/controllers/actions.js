64fd1ccdfd015441aa4b9840a6ba3634b3f8be74
Ansonhkg <ansonox@gmail.com>
2016-02-09 17:11:56 +0000

    Action page

--- app/scripts/controllers/actions.js
+++ app/scripts/controllers/actions.js
@@ -9,7 +9,15 @@
  */
 angular.module('jalpWebApp')
   .controller('ActionsCtrl', function ($scope, $rootScope, $firebaseArray) {
-    
-  	
-    
+
+    var ref = $rootScope.ref;
+    var authData = $rootScope.authData;
+
+    ref.onAuth(function(authData){
+      if(authData){
+        var UID = authData.uid;
+        var scriptReference = ref.child('users').child(UID).child('data').child('scripts');
+        $scope.scripts = $firebaseArray(scriptReference);
+      }
+    })
   });
