53f83b3e0e178eda41e983c33bbf01a378c19d41
Liam <lcottier94@googlemail.com>
2016-02-09 14:07:49 +0000

    Added sensor readings to room page

--- app/scripts/controllers/room.js
+++ app/scripts/controllers/room.js
@@ -8,10 +8,18 @@
  * Controller of the jalpWebApp
  */
 angular.module('jalpWebApp')
-  .controller('RoomCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('RoomCtrl', function ($scope,$rootScope, $firebaseArray) {
+    
+    var ref = $rootScope.ref;
+    var authData = $rootScope.authData;
+
+    ref.onAuth(function(authData){
+      if(authData){
+        var UID = authData.uid;
+        var userReference = ref.child('users').child(UID).child('data').child('spots');
+        var roomReference = ref.child('users').child(UID).child('data').child('rooms');
+
+        $scope.sensors = $firebaseArray(userReference);
+      }
+    })
   });
