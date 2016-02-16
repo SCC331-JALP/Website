62aef3d36542c9f32513e31b8c0d6d559b6bfd2d
Joshua Stennett <Josh@dyn-10-32-149-0.winet.local>
2016-02-16 15:51:26 +0000

    use Dev page code to display all users in ES page

--- app/scripts/controllers/emergency.js
+++ app/scripts/controllers/emergency.js
@@ -8,10 +8,25 @@
  * Controller of the jalpWebApp
  */
 angular.module('jalpWebApp')
-  .controller('EmergencyCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('EmergencyCtrl', function ($scope, $rootScope, $firebaseArray, $window) {
+
+    var ref = $rootScope.ref;
+    var authData = $rootScope.authData;
+    var isES = $rootScope.isES;
+
+    var users = authData ? $firebaseArray(ref.child('users')) : null;
+
+    users.$loaded().then(function () {
+      $scope.users = users;
+      //console.log(users);
+    })
+
+    $scope.isES = function(){
+      if(!isES){
+        $window.location.href = '/sensor';
+      }else{
+        return true;
+      }
+    }
+
   });
