f2dbbe063d7a075b296add773b497510372e6753
Liam <lcottier94@googlemail.com>
2016-02-17 15:58:51 +0000

    Added diet plan to fridge view

--- app/scripts/controllers/fridge.js
+++ app/scripts/controllers/fridge.js
@@ -8,10 +8,40 @@
  * Controller of the jalpWebApp
  */
 angular.module('jalpWebApp')
-  .controller('FridgeCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('FridgeCtrl', function ($scope, $rootScope, $firebaseArray) {
+    var ref = $rootScope.ref;
+    var authData = $rootScope.authData;
+    var isES = $rootScope.isES;
+
+    var UID = authData.uid;;
+
+    ref.onAuth(function(authData){
+      if(authData){
+      	var fridgeReference = ref.child('users').child(UID).child('data').child('fridge');
+      	$scope.diets = $firebaseArray(fridgeReference.child('diet'));
+
+      }
+    })
+
+    $scope.indexToFood = function(index){
+      //console.log(index);
+      switch(index){
+        case 0:
+          return "Vegetables";
+        case 1:
+          return "Fish";
+        case 2:
+          return "Meat";
+        case 3:
+          return "Chocolate";
+        case 4:
+          return "Pasta";
+        case 5:
+          return "Potatoes";
+        case 6:
+          return "Fruit";
+        case 7:
+          return "Biscuits";
+      }
+    }
   });
