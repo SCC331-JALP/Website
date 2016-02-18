91561507cea1c03ba7cc8ecc4a348575fd6fd204
Liam <lcottier94@googlemail.com>
2016-02-18 00:59:09 +0000

    Trying to get the contents of the fridge amounts

--- app/scripts/controllers/fridge.js
+++ app/scripts/controllers/fridge.js
@@ -47,35 +47,16 @@ angular.module('jalpWebApp')
       }
     }
 
-    $scope.idToFood = function(id){
-      console.log(id);
-      var idInt =  parseInt(id, 10);
-      console.log(idInt);
-      switch(idInt){
-        case 1:
-          return "Vegetables";
-        case 2:
-          return "Fish";
-        case 3:
-          return "Meat";
-        case 4:
-          return "Chocolate";
-        case 5:
-          return "Pasta";
-        case 6:
-          return "Potatoes";
-        case 7:
-          return "Fruit";
-        case 8:
-          return "Biscuits";
-        }
-      }
-
     $scope.percentageDrank = function(drank,toDrink){
-    		console.log(drank);
 	    	var percentage = (drank/toDrink) * 100;
 	    	percentage = Math.round(percentage);
 	    	var returner = percentage + "%";
 	    	return returner;
+	    }
+
+	$scope.getLength = function(content){
+			var contentRef = ref.child('users').child(UID).child('data').child('fridge').child('fridgeContents');
+    		var contentArray = $firebaseArray(contentRef);
+    		console.log(contentArray);
 	    }
   });
